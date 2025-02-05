aee58fc981992862651d44236a9fe4700767c673
Samy <chebreksamy@gmail.com>
2025-02-05 09:11:27 +0100

    [FEAT] : make the test of the function calculateTotalPrice

--- test/function.test.js
+++ test/function.test.js
@@ -29,3 +29,42 @@ describe("When I Check if it is even a number", () => {
 
 
 });
+
+
+describe("When I calculate the total price of the cart", () => {
+
+    test("When I return an array of prices then it should return the total price", () => {
+        expect(calculateTotalPrice([1, 2], 1.20)).toBe(6.60);
+    });
+
+    test("When I return an array of negative prices then it should return an error", () => {
+        expect(() => calculateTotalPrice([-1, -2, -3], 1.20)).toThrow(Error("Each price must be a non-negative number"));
+        ;
+    });
+
+    test("When I return a string then it should throw an error", () => {
+        expect(() => calculateTotalPrice("pr", 1.20)).toThrow(Error("Prices must be an array"));
+    });
+
+    test("When I return null then it should throw an error", () => {
+        expect(() => calculateTotalPrice(null, 1.20)).toThrow(Error("Prices must be an array"));
+    });
+
+    test("When I return a number then it should throw an error", () => {
+        expect(() => calculateTotalPrice(1, 1.20)).toThrow(Error("Prices must be an array"));
+    });
+
+    // test("When I return an empty array then it should throw an error", () => {
+    //     expect(() => calculateTotalPrice([], 1.20)).toThrow(Error("Each price must be a non-negative number"));
+    // });
+
+    test("When I return to taxrate null then it should throw an error", () => {
+        expect(() => calculateTotalPrice([1, 2], null)).toThrow(Error("Tax rate must be a number"));
+    });
+
+});
+
+
+describe("When I send a notification", () => {
+
+});
