c2d30c734ffdad0ef22b0db359c60613cb1a8c55
Samy <chebreksamy@gmail.com>
2025-02-05 09:03:50 +0100

    [FEAT] : make the the of the function isEven

--- test/function.test.js
+++ test/function.test.js
@@ -1,5 +1,31 @@
 const { isEven, calculateTotalPrice, processPurchase, sendNotification, generatePassword } = require("./../src/function.js");
 
-test("",() =>{
+
+
+describe("When I Check if it is even a number", () => {
+    test("When I return an even number then it should return true", () => {
+        expect(isEven(2)).toBe(true);
+    });
+
+    test("When I return an odd number then it should return false", () => {
+        expect(isEven(3)).toBe(false);
+});
+
+    test("When I return a string then it should throw an error", () => {
+        expect(() => isEven("pr")).toThrow(new Error("Input must be a number"));
+    });
+
+    test("When I return null then it should throw an error", () => {
+        expect(() => isEven(null)).toThrow(new Error("Input must be a number"));
+    });
+
+    test("When I return an even negative number then it should return true", () => {
+        expect(isEven(-2)).toBe(true);
+    });
+
+    test("When I return an odd negative number then it should return false", () => {
+        expect(isEven(-3)).toBe(false);
+    });
+
 
 });
